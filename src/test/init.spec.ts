83b784febdeaa8051fb2cb6c5cee913f58e68a29
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-07-16 20:48:40 -0300

    Unit tests with jest have been configured.

---
+++ src/test/init.spec.ts
@@ -0,0 +1,10 @@
+
+
+describe("This is a test ", () => {
+    it("This is the first test i wrote with jest", () => {
+        const num1 = 2;
+        const num2 = 2;
+
+        expect(num1 + num2).toBe(4);
+    })
+})
